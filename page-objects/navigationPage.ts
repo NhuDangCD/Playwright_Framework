53bd17b655fe60125cece56edb509151beb1bcb4
NhuDangCD <nhu.dang@conversiondigital.com.au>
2025-07-07 13:14:41 +0700

    firstcommit

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -1,6 +1,4 @@
 import { Locator, Page } from "@playwright/test";
-// This file defines a NavigationPage class that encapsulates navigation methods for a web application.
-
 export class NavigationPage {
     private readonly page: Page;
     readonly fromLayoutsMenuItem: Locator
@@ -32,23 +30,16 @@ export class NavigationPage {
 
     }
     async tooltipPage() {
-        await this.page.getByText('Modal & Overlays').click()
+        await this.selectgroupMenuItem('Modal & Overlays')
         await this.page.getByText('Tooltip').click()
 
     }
     async selectgroupMenuItem(groupItemTitle: string) {
         const menuItem = this.page.locator(`a[title="${groupItemTitle}"]`);
-
-        // Ensure the element is visible and ready for interaction
-        //await expect(menuItem).toBeVisible({ timeout: 10000 });  // Wait for visibility
-
-        // Check if the menu is expanded, if not, click to expand
         const expandedState = await menuItem.getAttribute('aria-expanded');
         if (expandedState === "false") {
             await menuItem.click();
         }
     }
-
-
 }
 
